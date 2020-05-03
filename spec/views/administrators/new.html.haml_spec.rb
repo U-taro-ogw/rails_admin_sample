18b83346d1e68604d9b1ddd324e6852829c0ed01
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-05-04 04:19:31 +0900

    setup sorcery

---
+++ spec/views/administrators/new.html.haml_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "administrators/new", type: :view do
+  before(:each) do
+    assign(:administrator, Administrator.new(
+      email: "MyString",
+      crypted_password: "MyString",
+      salt: "MyString"
+    ))
+  end
+
+  it "renders new administrator form" do
+    render
+
+    assert_select "form[action=?][method=?]", administrators_path, "post" do
+
+      assert_select "input[name=?]", "administrator[email]"
+
+      assert_select "input[name=?]", "administrator[crypted_password]"
+
+      assert_select "input[name=?]", "administrator[salt]"
+    end
+  end
+end
