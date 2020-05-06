8278e371f12bee55e5a4cb0079d2d00a7c4bb73d
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-05-06 20:10:32 +0900

    [wip] system spec

--- spec/system/administrator_sessions_spec.rb
+++ spec/system/administrator_sessions_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "AdministratorSessions", type: :system do
   it 'ログイン画面が表示されること' do
-    visit '/sessions/new'
+    visit login_path
+    expect(page).to have_content('Email')
   end
 end
--- spec/system/dashboards_spec.rb
+++ spec/system/dashboards_spec.rb
@@ -1,25 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Dashboards', type: :system do
+  before do
+    visit login_path
+    click_button 'Login'
+    fill_in 'administrator[email]', with: administrator.email
+    fill_in 'administrator[password]', with: administrator.password
+
+    wait 5
+
+    click_button 'Login'
+  end
+
   describe 'ダッシュボードページ' do
     context 'ロールA' do
-      let!(:role_A_admin) { create(:administrator, :role_A, email: 'role_A@example.com') }
+      let!(:administrator) { create(:administrator, :role_A, email: 'role_A@example.com') }
       it do
-        visit
+        expect(page).to have_content('Admin::Dashboards#index')
       end
     end
 
     context 'ロールB' do
-      let!(:role_B_admin) { create(:administrator, :role_B, email: 'role_B@example.com') }
+      let!(:administrator) { create(:administrator, :role_B, email: 'role_B@example.com') }
       it do
-        visit
+        expect(page).to have_content('Admin::Dashboards#index')
       end
     end
 
     context 'ロールC' do
-      let!(:role_C_admin) { create(:administrator, :role_C, email: 'role_C@example.com') }
+      let!(:administrator) { create(:administrator, :role_C, email: 'role_C@example.com') }
       it do
-        visit
+        expect(page).to have_content('Admin::Dashboards#index')
       end
     end
   end
