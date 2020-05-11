eed3ec12039edc2bad7dbea315ab852dae420fa4
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-05-12 01:31:04 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :administrators
+  # devise_for :administrators
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # resources :administrators_sessions, only: %i[create]
@@ -11,6 +11,18 @@ Rails.application.routes.draw do
   # get 'login' => 'administrator_sessions#new'
   # post 'login' => 'administrator_sessions#create'
   # delete 'logout' => 'administrator_sessions#destroy'
+
+  devise_for :administrators, :controllers => {
+      :registrations => 'administrators/registrations',
+      :sessions => 'administrators/sessions'
+  }
+
+  devise_scope :administrator do
+    get "administrator/:id", :to => "administrators/registrations#detail"
+    get "signup", :to => "administrators/registrations#new"
+    get "login", :to => "administrators/sessions#new"
+    get "logout", :to => "administrators/sessions#destroy"
+  end
 
   namespace :admin do
     get 'versions' => 'versions#index'
